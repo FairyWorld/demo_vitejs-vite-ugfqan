6820ad4795b08b28d5afa2a7b37bd4d757f1198c
FairyWorld <32956212+FairyWorld@users.noreply.github.com>
2023-07-02 18:42:46 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
+<!-- html模板 -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1>待办列表</h1>
+    <input type="text" v-model="newTodo" placeholder="输入待办事项" />
+    <button @click="addTodo">添加</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<!-- 业务逻辑 -->
+<script setup>
+import { ref } from 'vue';
+
+const newTodo = ref(''); // 响应式变量newTodo，表示输入框的值
+const todos = ref([]); // 响应式变量todos，表示待办列表
+
+const addTodo = () => {
+  if (newTodo.value) {
+    todos.value.push(newTodo.value); // 将输入的值添加到列表中
+    newTodo.value = ''; // 清空输入框的值
+  }
+};
+</script>
+
+<!-- 样式 -->
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f8f8f8;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+input {
+  padding: 5px 10px;
+  margin-right: 10px;
+  border: 1px solid #ddd;
+  border-radius: 3px;
+  font-size: 14px;
+}
+
+button {
+  padding: 5px 10px;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  font-size: 14px;
+  cursor: pointer;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 10px 0;
+  text-align: left;
+}
+
+li {
+  margin-bottom: 10px;
 }
 </style>
